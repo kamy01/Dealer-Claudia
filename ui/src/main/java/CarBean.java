7ac2d0efb1a8b949baa5a10072430029748b7949
claudia <claudia.dumea18@gmail.com>
2017-11-28 17:58:16 +0200

    add registration in the database for a car

--- ui/src/main/java/CarBean.java
+++ ui/src/main/java/CarBean.java
@@ -1,20 +1,39 @@
 import com.fortech.Car;
 import com.fortech.services.CarService;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import java.util.HashMap;
+import java.util.Map;
 
 @ManagedBean
 @RequestScoped
 public class CarBean {
     private Car car =  new Car();
-
+    private Map<String,String> colors;
+    private Map<String,String> conditions;
     @EJB
     private CarService carService;
 
-    public void addCar(){
+    @PostConstruct
+    public void init() {
+        colors = new HashMap<String, String>();
+        colors.put("red", "red");
+        colors.put("black", "black");
+        colors.put("blue", "blue");
+
+        conditions = new HashMap<String, String>();
+        conditions.put("new", "new");
+        conditions.put("used", "used");
+    }
+
+    public String addCar(){
+
         carService.addCar(car);
+        //fara ??
+        return "addCar?faces-redirect=true";
     }
 
     public Car getCar() {
@@ -23,5 +42,21 @@ public class CarBean {
 
     public void setCar(Car car) {
         this.car = car;
+    }
+
+    public Map<String, String> getColors() {
+        return colors;
+    }
+
+    public void setColors(Map<String, String> colors) {
+        this.colors = colors;
+    }
+
+    public Map<String, String> getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(Map<String, String> conditions) {
+        this.conditions = conditions;
     }
 }
